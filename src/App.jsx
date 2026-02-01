863b089fc761c0db78855c63d60e807287c5e992
Pragyan-P-Palai <pragyanpalai2615@gmail.com>
2026-02-01 22:57:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import InstagramLogin from './InstagramLogin'; // Import your new component
-import './App.css'; // Keep this if you want, or delete it
+import InstagramLogin from './InstagramLogin'; 
+import './App.css'; 
 
 function App() {
   return (
